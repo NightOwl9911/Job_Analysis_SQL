1bfdca9dec89a149b846ee450736fb1ddd01e7b1
Vladimir Parra <vlaidmir.business21@gmail.com>
2026-02-05 18:38:31 -0500

    Add part 3

--- sql_analysis/3_top_demanded_skills.sql
+++ sql_analysis/3_top_demanded_skills.sql
@@ -1,4 +1,8 @@
 /*
-Questions: What are the most in demands skills for data analysis jobs and separately the ones for data scientists jobs
+Questions: What are the most in demands skills for Data analysis jobs and separately the ones for Data scientists jobs
 - Join job postings to inner join table similar to query 2
-- Identify the top 7
+- Identify the top 10 in-demand skills for a Data analyst and Data scientist
+- Focus on all jobs postings
+- Why? Retrieves the top 10 skills with the highest demand in job market,
+    providing insights into the most valuable skills for job seekers.
+*/ 
